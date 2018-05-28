c460924833d120d446acc93dccf240eef6ffc64b
yamato48 <yamd1048@gmail.com>
2018-05-29 00:31:19 +0900

    Create UnityChanController
    キャラクターを動かすスクリプト作成

---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour {
+	//アニメーションするためのコンポーネントを入れる
+    private Animator myAnimator;
+	//Unityちゃんを移動させるコンポーネントを入れる
+    private Rigidbody myRigidbody;
+    //前進するための力
+    private float forwardForce = 800.0f;
+
+	// Use this for initialization
+    void Start()
+    {
+        //Animatorコンポーネントを取得
+        this.myAnimator = GetComponent<Animator>();
+
+        //走るアニメーションを開始
+        this.myAnimator.SetFloat("Speed", 1);
+         
+        //Rigidbodyコンポーネントを取得
+        this.myRigidbody = GetComponent<Rigidbody>();
+	}
+     
+	// Update is called once per frame
+	void Update () {
+		//Unityちゃんに前方向の力を加える
+        this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);	
+	}
+}

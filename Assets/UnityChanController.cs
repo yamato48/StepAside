e2dfaba03d8e7cdb14262efc7b006bf0097da70a
yamato48 <yamd1048@gmail.com>
2018-05-29 13:16:00 +0900

    Run right and left
    左右キーで左右に移動

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -9,8 +9,12 @@ public class UnityChanController : MonoBehaviour {
     private Rigidbody myRigidbody;
     //前進するための力
     private float forwardForce = 800.0f;
-
+	//左右に移動するための力
+    private float turnForce = 500.0f;
+    //左右の移動できる範囲
+    private float movableRange = 3.4f;
 	// Use this for initialization
+
     void Start()
     {
         //Animatorコンポーネントを取得
@@ -27,5 +31,13 @@ public class UnityChanController : MonoBehaviour {
 	void Update () {
 		//Unityちゃんに前方向の力を加える
         this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);	
+		//Unityちゃんを矢印キーまたはボタンに応じて左右に移動させる
+        if (Input.GetKey (KeyCode.LeftArrow)  && -this.movableRange < this.transform.position.x) {
+            //左に移動
+            this.myRigidbody.AddForce (-this.turnForce, 0, 0);
+        } else if (Input.GetKey (KeyCode.RightArrow)  && this.transform.position.x < this.movableRange) {
+            //右に移動
+            this.myRigidbody.AddForce (this.turnForce, 0, 0);
+        } 
 	}
 }
